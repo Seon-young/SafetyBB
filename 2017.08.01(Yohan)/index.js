90052fef0fb04c3a1f37173d49f25d39423c2d0d
dldygks25 <dldygks25@naver.com>
2017-08-07 15:32:38 +0900

    2017.08.07 version2
    version 2 (0807)

--- 2017.08.01(Yohan)/index.js
+++ 2017.08.01(Yohan)/index.js
@@ -5,6 +5,17 @@ var submitBtn = document.getElementById("submitBtn");
 var Customertype = document.getElementById("Customertype");
 var authProvider = new firebase.auth.GoogleAuthProvider();
 
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCWOhjgubSMNEavwmvC4QPNZOuzZ63Hyb4",
+    authDomain: "safetybb-65f64.firebaseapp.com",
+    databaseURL: "https://safetybb-65f64.firebaseio.com",
+    projectId: "safetybb-65f64",
+    storageBucket: "safetybb-65f64.appspot.com",
+    messagingSenderId: "909521276736"
+  };
+  firebase.initializeApp(config);
+
 auth = firebase.auth();
 database = firebase.database(); // DB 초기화
 
@@ -13,7 +24,6 @@ auth.onAuthStateChanged(function(user)
   if(user) {
     // 인증 성공시
     console.log("success");
-    console.log(user);
     userInfo = user;
   }
   else {
@@ -24,14 +34,11 @@ auth.onAuthStateChanged(function(user)
 
 
 function submitClick() {
-
   var firebaseRef = firebase.database().ref();
   var messageText = address.value;
   var CustypeText = Customertype.value;
+  firebaseRef.child("Users/" + userInfo.uid).child("address").set(messageText);
+  firebaseRef.child("Users/" + userInfo.uid).child("Customertype").set(CustypeText);
 
-  console.log(userInfo.uid);
-
-  firebaseRef.child(userInfo.uid).child("address").set(messageText);
-  firebaseRef.child(userInfo.uid).child("Customertype").set(CustypeText);
 
 }

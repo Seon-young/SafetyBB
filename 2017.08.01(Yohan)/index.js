579a7bcfc12067c16bd4f3993672b68aa437d476
dldygks25 <dldygks25@naver.com>
2017-09-03 23:22:18 +0900

    Add files via upload
    Version 3
    선영이 Google ID로 API 키 및 연결 확인

--- 2017.08.01(Yohan)/index.js
+++ 2017.08.01(Yohan)/index.js
@@ -1,5 +1,4 @@
-
-var userInfo,database, auth;
+var userInfo, database, auth;
 var address = document.getElementById("address");
 var submitBtn = document.getElementById("submitBtn");
 var Customertype = document.getElementById("Customertype");
@@ -7,28 +6,26 @@ var authProvider = new firebase.auth.GoogleAuthProvider();
 
   // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyCWOhjgubSMNEavwmvC4QPNZOuzZ63Hyb4",
-    authDomain: "safetybb-65f64.firebaseapp.com",
-    databaseURL: "https://safetybb-65f64.firebaseio.com",
-    projectId: "safetybb-65f64",
-    storageBucket: "safetybb-65f64.appspot.com",
-    messagingSenderId: "909521276736"
+    apiKey: "AIzaSyAX3zsgsAVgY2FoikSWi8Ou130mPv2RrFA",
+    authDomain: "testfirebase-88dad.firebaseapp.com",
+    databaseURL: "https://testfirebase-88dad.firebaseio.com",
+    projectId: "testfirebase-88dad",
+    storageBucket: "testfirebase-88dad.appspot.com",
+    messagingSenderId: "727840492650"
   };
   firebase.initializeApp(config);
-
+  
 auth = firebase.auth();
 database = firebase.database(); // DB 초기화
 
-auth.onAuthStateChanged(function(user)
-{
-  if(user) {
+auth.onAuthStateChanged(function(user) {
+  if (user) {
     // 인증 성공시
     console.log("success");
     userInfo = user;
-  }
-  else {
+  } else {
     auth.signInWithPopup(authProvider);
-    auth.signInWithRedirect(authProvider);
+
   }
 });
 
